2b47f852b4c36ff19ddac9b4d00589cacad600e2
firulay <jhalexander18@hotmail.com>
2023-06-18 11:52:31 -0500

    error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://elladodelbien.github.io/sena2023"
+  base:"/sena2023"
 })
