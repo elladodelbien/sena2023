1e1a205140898de3056df0ea081187653447687f
firulay <jhalexander18@hotmail.com>
2023-06-18 20:09:46 -0500

    vitte.config corregido

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
+
+
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
+        home: resolve(__dirname, "pages/Home/index.html"),
+        ratting: resolve(__dirname, "pages/Ratting/index.html"),
+        edades: resolve(__dirname, "pages/Edades/index.html"),
+      },
+    },
+  },
+});
+
